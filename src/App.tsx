1416f3998885044df45db1a3bdac2bd5ff65f3ff
Toghrul Aghali <aghalitg@gmail.com>
2024-02-13 23:25:33 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+let pizza: string = 'Salami'
+function App() {
+  return (
+    <div className="App">
+      New App!
+    </div>
+  );
+}
+
+export default App;
